a12f519a5fdff15afe584160faf8dbd6dbbb3693
Johnny Howe <jonathon.o.howe@gmail.com>
2021-11-29 11:05:46 +1300

    Mouse cell highlighted in game

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 import pygame
 from window import Window
 
+GAME_GRID_SIZE = (10, 5)
+TOWER_SELECTOR_HEIGHT = 2
+
+
+def get_cell_pixel_position(cell_position):
+    scale = get_cell_pixel_size()
+    game_display_size = GAME_GRID_SIZE[0] * scale, GAME_GRID_SIZE[1] * scale
+    game_display_position = (
+        (Window.size[0] - game_display_size[0]) / 2,
+        (Window.size[1] - game_display_size[1] -
+         scale * TOWER_SELECTOR_HEIGHT) / 2
+    )
+    return game_display_position[0] + cell_position[0] * scale, game_display_position[1] + cell_position[1] * scale
+
+
+def get_cell_pixel_size():
+    return min(Window.size[0] / GAME_GRID_SIZE[0], Window.size[1] / (GAME_GRID_SIZE[1] + TOWER_SELECTOR_HEIGHT))
+
+
+def get_cell_pixel_rect(cell_position):
+    return get_cell_pixel_position(cell_position) + (get_cell_pixel_size(),) * 2
+
+
+def get_pixel_cell_position(pixel_position):
+    top_left = get_cell_pixel_position((0, 0))
+    scale = get_cell_pixel_size()
+    cell_position = (
+        int((pixel_position[0] - top_left[0]) / scale),
+        int((pixel_position[1] - top_left[1]) / scale)
+        )
+    if (cell_position[0] < 0 or cell_position[0] > GAME_GRID_SIZE[0] - 1 or
+        cell_position[1] < 0 or cell_position[1] > GAME_GRID_SIZE[1] - 1 or
+        pixel_position[0] < top_left[0] or
+        pixel_position[1] < top_left[1]):
+        return -1, -1
+    return cell_position
+
 
 def main():
 
     pygame.init()
 
-    game_grid_size = (10, 5)
-    
     while True:
 
         for event in pygame.event.get():
@@ -21,15 +56,24 @@ def main():
 
         Window.surface.fill((100, 100, 100))
 
-        scale = min(Window.size[0] / game_grid_size[0], Window.size[1] / (game_grid_size[1] + 1))
-        
-        game_display_size = game_grid_size[0] * scale, game_grid_size[1] * scale
-        game_display_position = ((Window.size[0] - game_display_size[0]) / 2, (Window.size[1] - game_display_size[1] - scale) / 2)
-        pygame.draw.rect(Window.surface, (255, 255, 255), game_display_position + game_display_size)
+        scale = get_cell_pixel_size()
 
-        selector_display_size = (game_display_size[0], scale)
-        selector_display_position = (game_display_position[0], game_display_position[1] + game_display_size[1])
-        pygame.draw.rect(Window.surface, (200, 200, 200), selector_display_position + selector_display_size)
+        game_display_size = GAME_GRID_SIZE[0] * \
+            scale, GAME_GRID_SIZE[1] * scale
+        game_display_position = ((Window.size[0] - game_display_size[0]) / 2,
+                                 (Window.size[1] - game_display_size[1] - scale * TOWER_SELECTOR_HEIGHT) / 2)
+        pygame.draw.rect(Window.surface, (255, 255, 255),
+                         game_display_position + game_display_size)
+
+        selector_display_size = (
+            game_display_size[0], scale * TOWER_SELECTOR_HEIGHT)
+        selector_display_position = (
+            game_display_position[0], game_display_position[1] + game_display_size[1])
+        pygame.draw.rect(Window.surface, (200, 200, 200),
+                         selector_display_position + selector_display_size)
+
+        if pygame.mouse.get_focused():
+            pygame.draw.rect(Window.surface, (0, 255, 0), get_cell_pixel_rect(get_pixel_cell_position(pygame.mouse.get_pos())))
 
         pygame.display.update()
 
