254fd23b332bad4bfef909d9a5d007275db58fe9
Johnny Howe <jonathon.o.howe@gmail.com>
2021-11-29 19:33:12 +1300

    Boring walls

--- wall.py
+++ wall.py
@@ -33,13 +33,12 @@ SPRITE_POSITIONS = [
 class Wall:
     def __init__(self, position):
         self.position = position
-        self.sprite_sheet = SpriteSheet(SHEET_PATH, (16, 1))
+        self.sprite_sheet = SpriteSheet(SHEET_PATH, (17, 1))
     
     def get_image(self, board):
-        left = not board.is_empty((self.position[0] - 1, self.position[1]))
-        right = not board.is_empty((self.position[0] + 1, self.position[1]))
-        up = not board.is_empty((self.position[0], self.position[1] - 1))
-        down = not board.is_empty((self.position[0], self.position[1] + 1))
-        key = (int(up), int(down), int(left), int(right))
-
-        return self.sprite_sheet.get_sprite_at((SPRITE_POSITIONS.index(key), 0))
+        # left = not board.is_empty((self.position[0] - 1, self.position[1]))
+        # right = not board.is_empty((self.position[0] + 1, self.position[1]))
+        # up = not board.is_empty((self.position[0], self.position[1] - 1))
+        # down = not board.is_empty((self.position[0], self.position[1] + 1))
+        # key = (int(up), int(down), int(left), int(right))
+        return self.sprite_sheet.get_sprite_at((16, 0))
