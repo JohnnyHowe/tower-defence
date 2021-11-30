28bb3a67fa5dd74b2ebffea16c4ea7b487abd6eb
Johnny Howe <jonathon.o.howe@gmail.com>
2021-11-30 14:31:57 +1300

    refactor + fix: pathfinder class + actually a star

--- game_grid.py
+++ game_grid.py
@@ -1,14 +1,78 @@
 
+class GridPathFinder:
+    last_board_id: str
+    path: list
+
+    def __init__(self, board):
+        self.board = board
+        self.last_board_id = self.get_board_id()
+        self.update_path()
+
+    def get_board_id(self):
+        """ Just a hash of the contents of the board """
+        return hash(str(self.board.items))
+
+    def get_path(self):
+        new_id = self.get_board_id()
+        if self.last_board_id != new_id:
+            self.update_path()
+            self.last_board_id = new_id
+        return self.path
+
+    def update_path(self):
+        self.path = self.generate_path()
+
+    def generate_path(self):
+        candidates = [(-1, y, None, 0) for y in range(self.board.size[1])]
+        explored = []
+        final_node = None
+
+        def h(x): return abs(x[0] - self.board.size[0])
+        def c(x): return x[3]
+        def f(x): return c(x) + h(x)
+
+        while len(candidates) > 0:
+            # Sort candidates by h
+            candidates.sort(key=f)
+
+            candidate = candidates.pop(0)
+            explored.append(candidate[:2])
+
+            # If candidate is a goal
+            if candidate[0] >= self.board.size[0] - 1:
+                final_node = candidate
+                break
+
+            # Else, add neighbours to candidates
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                neighbour_pos = (candidate[0] + dx, candidate[1] + dy)
+                neighbour = neighbour_pos + (candidate, candidate[3] + 1)
+
+                # If neighbour is valid and unexplored
+                if self.board.is_on_grid(neighbour_pos) and self.board.is_empty(neighbour_pos) and neighbour_pos not in explored:
+                    candidates.append(neighbour)
+
+        if final_node is None:
+            return None
+
+        # Construct path/backtrack
+        path = [final_node[:2]]
+        while final_node is not None:
+            path.append(final_node[:2])
+            final_node = final_node[2]
+
+        return list(reversed(path)) 
+
 class GameGrid:
 
     items: list
     size: tuple
-    path = []
-    grid_changed = True     # Flag set when board has changed and path is outdated
+    pathfinder: GridPathFinder
 
     def __init__(self, size: tuple):
         self.items = [None, ] * size[0] * size[1]
         self.size = size
+        self.pathfinder = GridPathFinder(self)
 
     def get_at(self, position: tuple):
         if not self.is_on_grid(position): return None
@@ -17,7 +81,6 @@ class GameGrid:
     def set_at(self, position, obj):
         if self.items[self.get_index(position)] != obj:
             self.items[self.get_index(position)] = obj
-            self.grid_changed = True
 
     def is_empty(self, position):
         if not self.is_on_grid(position): return False
@@ -35,53 +98,6 @@ class GameGrid:
     def clear_at(self, position):
         if self.items[self.get_index(position)] is not None:
             self.set_at(position, None)
-            self.grid_changed = True
 
     def get_path(self):
-        if self.grid_changed:
-            self.update_path()
-            self.grid_changed = False
-        return self.path
-
-
-    def update_path(self):
-        self.path = self.generate_path()
-        
-    def generate_path(self):
-        candidates = [(-1, y, None) for y in range(self.size[1])]
-        explored = []
-        final_node = None
-
-        def h(x): return abs(x[0] - self.size[0])
-
-        while len(candidates) > 0:
-            # Sort candidates by h
-            candidates.sort(key=h)
-
-            candidate = candidates.pop(0)
-            explored.append(candidate[:2])
-
-            # If candidate is a goal
-            if candidate[0] >= self.size[0] - 1:
-                final_node = candidate
-                break
-
-            # Else, add neighbours to candidates
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                neighbour_pos = (candidate[0] + dx, candidate[1] + dy)
-                neighbour = neighbour_pos + (candidate,)
-
-                # If neighbour is valid and unexplored
-                if self.is_on_grid(neighbour_pos) and self.is_empty(neighbour_pos) and neighbour_pos not in explored:
-                    candidates.append(neighbour)
-
-        if final_node is None:
-            return None
-
-        # Construct path/backtrack
-        path = [final_node[:2]]
-        while final_node is not None:
-            path.append(final_node[:2])
-            final_node = final_node[2]
-
-        return list(reversed(path))
+        return self.pathfinder.get_path()
