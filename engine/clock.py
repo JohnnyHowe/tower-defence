64cf07a42c2ca816670d747e4077b2e6d3cbdcf8
Johnny Howe <jonathon.o.howe@gmail.com>
2021-12-07 07:15:47 +1300

    max dt

--- engine/clock.py
+++ engine/clock.py
@@ -6,6 +6,7 @@ class _Clock:
     last_frame_time: float
     dt: float       # seconds
     min_dt = 0.00001  # 1 ms
+    max_dt = 0.5
 
     def __init__(self):
         self.start_time = time.time()
@@ -14,7 +15,7 @@ class _Clock:
 
     def update(self):
         now = time.time()
-        self.dt = max(self.min_dt, now - self.last_frame_time)
+        self.dt = min(max(self.min_dt, now - self.last_frame_time), 0.5)
         self.last_frame_time = now
 
 
